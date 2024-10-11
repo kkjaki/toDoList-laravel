d973f6cd9c939b085096a68810b7ebd329778756
zaka <zakaria.harsoyo@gmail.com>
2024-10-11 08:53:42 +0700

    Add auth and crud

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Auth;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('users', UserController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::resource('tasks', TaskController::class);
+});
